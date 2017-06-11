3f27b731e081f465cefb585504b4243e6d7ca424
paubins <paubin12@gmail.com>
2017-06-10 22:28:50 -0700

    Initial Commit

---
+++ GifPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GifPlayer
+//
+//  Created by Patrick Aubin on 6/10/17.
+//  Copyright © 2017 com.paubins.GifPlayer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
